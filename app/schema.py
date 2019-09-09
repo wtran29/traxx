11c4f60d62e478a124dd077dcd7923d7b175071f
William Tran <wtran29@gmail.com>
2019-09-08 18:26:55 -0700

    setup query for individual user, config graphql_jwt for token auth

--- app/schema.py
+++ app/schema.py
@@ -1,15 +1,18 @@
 import graphene
+import graphql_jwt
 
 import tracks.schema
 import users.schema
 
 
-class Query(tracks.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, tracks.schema.Query, graphene.ObjectType):
     pass
 
 
 class Mutation(users.schema.Mutation, tracks.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
